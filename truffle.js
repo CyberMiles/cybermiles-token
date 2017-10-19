ea80319ed1c1d411a3a0dd0f98911a2caa010982
dragon <longzhi@gmail.com>
2017-10-19 17:05:31 +0800

    Modify truffle config

--- truffle.js
+++ truffle.js
@@ -9,6 +9,12 @@ module.exports = {
       host: "localhost",
       port: 8545,
       network_id: 3 // Ropsten
+    },
+    live: {
+      host: "localhost",
+      port: 8545,
+      network_id: 1,
+      from: "0x02e21934252d43fdcea032ed2abb2c963033a950"
     }
   }
 };
