9a56180fa5f30dcf4b6e661060b8bfc99a90943e
dragon <longzhi@gmail.com>
2017-10-20 11:56:10 +0800

    Set default testnet address

--- truffle.js
+++ truffle.js
@@ -8,7 +8,10 @@ module.exports = {
     testnet: {
       host: "localhost",
       port: 8545,
-      network_id: 3 // Ropsten
+      network_id: 3, // Ropsten,
+      from: "0x93B6D9d35D3a845c77d22FAB566f0A74c92B897d",
+      gas: 4712388,
+      gasPrice: 20000000000
     },
     live: {
       host: "localhost",
